ef8108bc8f8a2c97b46e5f2733ace441f61f9f83
Vladyslav Kotko <kotko@macbook.local>
2019-01-17 10:51:31 +0200

    fix

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-  console.log('reday')
+  jQuery('body').on('submit', '.form-login', function(e){
+     e.preventDefault();
+     var form = $(this).serialize();
+       jQuery.ajax({
+         type: 'POST',
+         url: 'https://kotko.github.io/spotify-test/',
+         data: {
+           action:  'loginSpotify',
+           data: form
+         },
+         beforeSend: function() {
+         },
+         success: function(response) {
+         }
+       });
+   });
 })
